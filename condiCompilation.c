674ee4422ada1e79da94006e9d128fd9e44c64ad
xianling zhang <xianlingzhang6@gmail.com>
2016-11-07 19:10:08 -0500

    Create condiCompilation.c

---
+++ condiCompilation.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+#define grade 15000
+
+void main()
+{
+#if grade <=15000
+printf();
+#elif grade>15000&&grade<20000
+printf();
+#else
+printf();
+#endif
+getch();
+}
