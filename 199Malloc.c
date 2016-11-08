2f39a7ada68c6c61212f00d011ccbd6402f09eff
xianling zhang <xianlingzhang6@gmail.com>
2016-11-07 21:31:14 -0500

    Update 199Malloc.c

--- 199Malloc.c
+++ 199Malloc.c
@@ -1,4 +1,18 @@
 /*Demonstrate the malloc() function*/
+//dynamic memory allocation
+/*
+stack: store local variables
+heap: dynamic memory allocation
+data : store global static variables
+code: store all instructions
+
+four functions:
+malloc(); eg ptr=(data-type *)malloc(byte-size wanted)
+calloc();
+realloc();
+free();
+use stdlib.h
+*/
 #include<stdlib.h>
 #include<stdio.h>
 #include<conio.h>
@@ -10,7 +24,8 @@ clrscr();
 printf("Enter the size of array!");
 scanf("%d",&a);//store the array size with a
 
-p=(int *)malloc(a*sizeof(int));//??
+p=(int *)malloc(a*sizeof(int));// eg ptr=(data-type *)malloc(byte-size wanted)
+  //two bytes for integer, int return 2
 
 if(p==NULL)
 {
@@ -20,12 +35,22 @@ printf("The Memory Allocation fails!");
 for(i=0;i<a;i++)
 {
 printf("Enter value for p[%d]",i);
-scanf("%d",&p[i]);
+scanf("%d",&p[i]);//store values to the allocated memory
 }
 //use for loop to print out the memory address
 for(i=0;i<a;i++)
 {
 printf("\n Value %d Memory address %u",p[i],&p[i]);
+  //print out the memory address
+  /*
+  Enter!: 2
+  Enter for p[0]: 12
+  p[1]:14
+  
+  value 12 memory address 1942
+  value 14 memory address 1944
+  //2 bytes distance
+  */
 }
 
 getch();
