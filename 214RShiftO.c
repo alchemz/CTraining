784781e4b6029277f976e6c2460056a87751fbc8
xianling zhang <xianlingzhang6@gmail.com>
2016-11-08 14:54:45 -0500

    Create 214RShiftO.c

---
+++ 214RShiftO.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int a, b;
+printf("Enter the value for a");
+scanf("%d",a);
+b=a>>2; //right shift for two blocks
+printf("The b value is %d",b);
+getch();
+}
