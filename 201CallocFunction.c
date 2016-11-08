0e9d568c8d57fbca031e71c57c77d197e42b701c
xianling zhang <xianlingzhang6@gmail.com>
2016-11-07 21:41:48 -0500

    Update 201CallocFunction.c

--- 201CallocFunction.c
+++ 201CallocFunction.c
@@ -1,7 +1,7 @@
 /*
 Dynamic Memory Allocation
 calloc();
-ptr=(data-type)calloc(size, sizeof(byte wanted));
+ptr=(data-type)calloc(enteredsize, sizeof(byte wanted));
 */
 #include<stdlib.h>
 #include<stdio.h>
@@ -13,17 +13,26 @@ int a,i,*p;
 clrscr();
 printf("Enter the array size");
 scanf("%d",&a);
+  //p=(int *)calloc(a*sizeof(int));
+  //allocate with some gabage space
 p=(int *)calloc(a,sizeof(int));
+
+  //no entered value, 0 stored
 if(p==NULL)
 {
 printf("Memory Allocation Fails!");
 }
+  
+  //enter and store values
 for(i=0;i<a;i++)
 {
 printf("Enter value of p[i]",p[i]);
 scanf("%d",&p[i]);
 }
+  
 free(p);
+  //free the allocated memory 
+  
 for(i=0;i<a;i++)
 {
 printf("\n Vlaue %d Memory Address %u",p[i],&p[i]);
