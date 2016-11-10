986aebe573f4e1f9974327c8d4535cf8288949b6
xianling zhang <xianlingzhang6@gmail.com>
2016-11-10 12:34:09 -0500

    Update 220BitwiseO2.c

--- 220BitwiseO2.c
+++ 220BitwiseO2.c
@@ -3,12 +3,17 @@
 
 void main()
 {
+  //bit pattern for 32: 100000
 int a=32, b;
 //use 32 to check 5th bit
 clrscr();
-b=a|2;
-printf("%d",b);
 b=a|1;
+  //turn on the first bit
+  //100001 33
+printf("%d",b);
+b=a|2;
+  //turn on the second bit
+  //100010 34
 printf("%d",b);
 getch;
 }
