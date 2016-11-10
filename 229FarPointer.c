2eb09ea97439b65f06b1b00a9c11be8d845ae266
xianling zhang <xianlingzhang6@gmail.com>
2016-11-10 17:12:05 -0500

    Create 229FarPointer.c

---
+++ 229FarPointer.c
@@ -0,0 +1,15 @@
+#include<stdio.>
+#include<conio.h>
+
+void main()
+{
+
+int huge *p;
+int x=200;
+clrscr();
+p=&x;
+printf("\nThe size of huge pointer is %d byter",sizeof(p));
+printf("\nThe Memory Address %Fp",p;);
+getch();
+
+}
