fbe72e4f50628917d5557233f0adad912785b42b
xianling zhang <xianlingzhang6@gmail.com>
2016-11-10 12:19:59 -0500

    Create 218BitwiseO.c

---
+++ 218BitwiseO.c
@@ -0,0 +1,30 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+/*
+Bitwise AND operator: &
+X=A & 32
+check the bit on/off
+1-on
+0-off
+check from the right to left 5th is 0, off
+65-1000001
+32- 100000
+
+bit number: n
+value used to test 2^n
+*/
+
+void main()
+{
+int i=65,j;
+clrscr();
+printf("\n Vlaue of i=%d",i);
+j=i&1;
+if(j==0)
+printf("\tIt's first bit is off");
+else
+printf("Itis first bit is on");
+getch;
+}
