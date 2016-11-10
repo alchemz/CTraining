2cc4d9122e8eb493a679bf40df8a1c17743abb44
xianling zhang <xianlingzhang6@gmail.com>
2016-11-10 17:34:07 -0500

    Update 225NearPointer.c

--- 225NearPointer.c
+++ 225NearPointer.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 #include<conio.h>
-
+/*
+Near pointer only access to data segment in row 8
+*/
 void main()
 {
 int near *p;
 int x=200;
 clrscr();
+  //assign the x address to p
 p=&x;
 printf("The size of near pointer is %d",sizeof(p));
 getch();
