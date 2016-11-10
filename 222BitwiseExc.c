275f4f85735056d033b19dd1206cd0e32d58126b
xianling zhang <xianlingzhang6@gmail.com>
2016-11-10 12:39:00 -0500

    Update 222BitwiseExc.c

--- 222BitwiseExc.c
+++ 222BitwiseExc.c
@@ -4,10 +4,14 @@
 void main()
 {
 int a=32;
+  //bit pattern 100000
 clrscr();
 a=a^4;
+  //first turn on the third bit
+  //36
 printf("\n%d",a);
 a=a^4;
+  //then turn off the third bit
 printf("\n%d",a);
 getch();
 }
