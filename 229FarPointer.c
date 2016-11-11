e98751ccecc1994ce1d9fe6618c535f931faba74
xianling zhang <xianlingzhang6@gmail.com>
2016-11-10 20:04:25 -0500

    Update 229FarPointer.c

--- 229FarPointer.c
+++ 229FarPointer.c
@@ -4,12 +4,13 @@
 void main()
 {
 
-int huge *p;
+int far *p;
+  //same as huge pointer to access all 16 segments
 int x=200;
 clrscr();
 p=&x;
 printf("\nThe size of huge pointer is %d byter",sizeof(p));
 printf("\nThe Memory Address %Fp",p;);
 getch();
-
+//x
 }
