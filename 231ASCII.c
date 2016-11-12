6e9b32fa64253524ab9a4bff3924c2ff0f2a3566
xianling zhang <xianlingzhang6@gmail.com>
2016-11-12 18:37:44 -0500

    Create 231ASCII.c

---
+++ 231ASCII.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int i;
+for(i=1;i<255;i++)
+{
+printf("\n The ASCII Value of \t %c is %d",i,i);
+getch();
+//%c is for printing out
+//%d is for printing out the value
+}
+}
