43702ddec60ad81c0a685845f8ce0c9a8d2af67a
xianling zhang <xianlingzhang6@gmail.com>
2016-11-12 18:46:16 -0500

    Create 234ShiftToggle.c

---
+++ 234ShiftToggle.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+
+void main()
+{
+char far*key;
+/*The key is pointing to memory address 0x417 where is monitors the status
+of toggle keys where they are on/off*/;
+key=(char far*)0x417;
+
+while(1)
+{
+clrscr();
+printf("\nPress ALT key to quit");
+if(*key&1)
+printf("\nRight shift is pressed");
+if(*key&2)
+printf("\nLeft shift is pressed");
+if(*key&4)
+printf("\nCtrl Lock is pressed");
+/*
+Setting this key to quit when alt key is pressed;
+*/
+if(*key&8)
+break;
+if(*key&16)
+printf("\nScroll lock is activated");
+if(*key&32)
+printf("\nNum lock is activated");
+if(*key&64)
+printf("\nCaps lock is activated");
+if(*key&128)
+printf("\nInset is activated");
+delay(99);
+}
+}
