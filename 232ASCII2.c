0422de4c9e2b474d819554e0e81ca07b73b744f7
xianling zhang <xianlingzhang6@gmail.com>
2016-11-12 19:49:09 -0500

    Update 232ASCII2.c

--- 232ASCII2.c
+++ 232ASCII2.c
@@ -4,6 +4,7 @@
 void main()
 {
 char ch;
+  //declare a character
 clrscr();
 printf("Press Any key on keyboard");
 ch=getch();
@@ -11,7 +12,12 @@ printf("\nAscii code=%d",ch);
 if(ch==0)
 {
 ch=getch();
+  //return the corresponding scan code
 printf("\nscan code =%d",ch);
 }
 getch();
+  
+  //press any key on keyboard ! F5
+  //Ascii code=0
+  //Scan code =60
 }
