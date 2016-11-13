b375235a90d88d235402eaab3fb8b2584150fcd2
xianling zhang <xianlingzhang6@gmail.com>
2016-11-12 19:59:27 -0500

    Update 234ShiftToggle.c

--- 234ShiftToggle.c
+++ 234ShiftToggle.c
@@ -1,19 +1,24 @@
 #include<stdio.h>
 #include<conio.h>
 #include<dos.h>
-
+/*
+left shift
+right shift
+Byte address 0x417
+*/
 void main()
 {
 char far*key;
 /*The key is pointing to memory address 0x417 where is monitors the status
 of toggle keys where they are on/off*/;
-key=(char far*)0x417;
+key=(char far*)0x417;//point to a address
 
 while(1)
 {
 clrscr();
 printf("\nPress ALT key to quit");
 if(*key&1)
+  //1&1=1 both bits are 1 so it returns 1
 printf("\nRight shift is pressed");
 if(*key&2)
 printf("\nLeft shift is pressed");
