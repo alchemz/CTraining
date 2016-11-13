7972201040d4ec64e6d9d9a8bf9fbf2fb4a57285
xianling zhang <xianlingzhang6@gmail.com>
2016-11-12 20:10:36 -0500

    Update 236Calculator.c

--- 236Calculator.c
+++ 236Calculator.c
@@ -6,33 +6,33 @@ void main()
 char ch;
 int a,b;
 clrscr();
-printf("\nEnter 1 for Addition");
-printf("\nEnter 2 for Substraction");
-printf("\nEnter 3 for Multiplication");
-printf("\nEnter 4 for Division");
+printf("\nEnter A for Addition");
+printf("\nEnter S for Substraction");
+printf("\nEnter M for Multiplication");
+printf("\nEnter D for Division");
 printf("\nEnter yourb choice");
 scanf("%c",&ch);
 switch()
 {
-case '1':
+case 'A':
         printf("\n Enter any two values");
         scanf("%d %d",&a,&b);
         a=a+b;
         printf("\nThe addition of two numbers is %d",a);
         break;
-case '2':
+case 'S':
        printf("\n Enter any two values");
         scanf("%d %d",&a,&b);
         a=a-b;
         printf("\nThe substraction of two numbers is %d",a);
         break;
-case '3':
+case 'M':
        printf("\n Enter any two values");
         scanf("%d %d",&a,&b);
         a=a*b;
         printf("\nThe multiplication of two numbers is %d",a);
         break;
-case '4':
+case 'D':
        printf("\n Enter any two values");
         scanf("%d %d",&a,&b);
         a=a/b;
