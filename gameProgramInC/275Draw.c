122a28b3ec325fc57a0d512780c64d68051655e4
xianling zhang <xianlingzhang6@gmail.com>
2017-01-02 18:03:38 -0500

    Update 275Draw.c

--- gameProgramInC/275Draw.c
+++ gameProgramInC/275Draw.c
@@ -1,4 +1,17 @@
-/* */
+/*
+filestyle()
+setbkcolor()
+setfillstyle()
+floodfill()
+rectangle()
+circle()
+line()
+putimage(x,y,buffer,XOR_PUT)
+getimage(x,y,x,y,buffer)
+imagesize()
+buffer=imagesize()
+
+*/
 
 #include<stdio.h>
 
@@ -22,6 +35,7 @@ void main()
 
 int gd=DETECT,gm,i,j,IMG1;
 
+  //define an empty pointer
 void *BUFFER1;
 
 initgraph(&gd,&gm,"C:\\turboc3\\bgi");
@@ -34,26 +48,26 @@ setbkcolor(2);
 
 rectangle(0,0,60,22);
 
+//fill blue color
 setfillstyle(1,9);
-
+//fill the region with the blue color
 floodfill(2,2,15);
 
+  //caculate the image size
 IMG1=imagesize(0,0,60,22);
 
-BUFFER1=malloc(IMG1);
+BUFFER1=malloc(IMG1);//allocate the image size
 
 getimage(0,0,60,22,BUFFER1);
-
+//XOR_PUT source image with the image already onscreen
 putimage(0,0,BUFFER1,XOR_PUT);
-
+//calculate the bricks position
 for(j=0;j<180;j+=30)
-
+//8 here is to make the bricks displayed at the center
 for(i=8;i<600;i+=70)
 
 {
-
 putimage(i,30+j,BUFFER1,XOR_PUT);
-
 }
 
 /* Drawing the Paddle */
@@ -87,9 +101,11 @@ getch();
 void print_line()
 
 {
-
+//background color
 setcolor(WHITE);
-
+//horizontal line
+  
+  //draw a larger stroke line
 line(0,18,640,18);
 
 line(0,19,640,19);
